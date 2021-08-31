7ce959574f201d065050e1e0a4fe4b0bb9756ad4
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-31 22:26:56 +0700

    Fix committer name, refactor

--- lib/profile_updater.ex
+++ lib/profile_updater.ex
@@ -9,28 +9,35 @@ defmodule ProfileUpdater do
   """
   def hello(access_token) do
     client = Tentacat.Client.new(%{access_token: access_token})
-    {200, data, _res} = Tentacat.Users.find(client, "narze")
+    {200, data, _res} = Tentacat.Users.me(client)
+
+    name = data |> get_in(["name"])
+    email = data |> get_in(["email"])
+    login = data |> get_in(["login"])
+    follower_count = data |> get_in(["followers"])
+    following_count = data |> get_in(["following"])
 
     # Get current sha of readme
-    {200, file, _res} = Tentacat.Contents.find(client, "narze", "profile-updater", "README.md")
+    {200, file, _res} = Tentacat.Contents.find(client, login, "profile-updater", "README.md")
     sha = file |> get_in(["sha"])
 
     # Update readme
-    username = data |> get_in(["login"])
-    follower_count = data |> get_in(["followers"])
-
     content =
-      ["# ProfileUpdater", "My name is #{username}, and I have #{follower_count} followers"]
+      ["# ProfileUpdater", "My name is #{login}, and I have #{follower_count} followers, I also followed #{following_count} users."]
       |> Enum.join("\n\n")
       |> Base.encode64()
 
     body = %{
       "message" => "Update README.md",
       "content" => content,
+      "committer" => %{
+        "name"  => name,
+        "email" => email,
+      },
       "sha" => sha,
       "branch" => "main"
     }
 
-    Tentacat.Contents.update(client, "narze", "profile-updater", "README.md", body)
+    Tentacat.Contents.update(client, login, "profile-updater", "README.md", body)
   end
 end
