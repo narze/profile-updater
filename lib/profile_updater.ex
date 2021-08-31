c2b123b667e8ca7b20afcf7e8d10f83751756328
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-31 22:19:17 +0700

    test: Update readme of this repo

--- lib/profile_updater.ex
+++ lib/profile_updater.ex
@@ -7,7 +7,30 @@ defmodule ProfileUpdater do
   Hello world.
 
   """
-  def hello do
-    IO.puts("Hello, world!")
+  def hello(access_token) do
+    client = Tentacat.Client.new(%{access_token: access_token})
+    {200, data, _res} = Tentacat.Users.find(client, "narze")
+
+    # Get current sha of readme
+    {200, file, _res} = Tentacat.Contents.find(client, "narze", "profile-updater", "README.md")
+    sha = file |> get_in(["sha"])
+
+    # Update readme
+    username = data |> get_in(["login"])
+    follower_count = data |> get_in(["followers"])
+
+    content =
+      ["# ProfileUpdater", "My name is #{username}, and I have #{follower_count} followers"]
+      |> Enum.join("\n\n")
+      |> Base.encode64()
+
+    body = %{
+      "message" => "Update README.md",
+      "content" => content,
+      "sha" => sha,
+      "branch" => "main"
+    }
+
+    Tentacat.Contents.update(client, "narze", "profile-updater", "README.md", body)
   end
 end
